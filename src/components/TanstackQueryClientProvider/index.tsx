466c59af68747e0330cca951499c4f9608ffc2c5
sjoleee <cent7425@gmail.com>
2024-05-05 23:32:35 +0900

    fix: ssr queryClient

--- src/components/TanstackQueryClientProvider/index.tsx
+++ src/components/TanstackQueryClientProvider/index.tsx
@@ -4,14 +4,31 @@ import { type PropsWithChildren } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: { retry: 0, refetchOnWindowFocus: false },
-    mutations: { retry: 0 },
-  },
-});
+const makeQueryClient = (): QueryClient => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = (): QueryClient => {
+  if (typeof window === "undefined") {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+
+    return browserQueryClient;
+  }
+};
 
 const TanstackQueryClientProvider = ({ children }: PropsWithChildren) => {
+  const queryClient = getQueryClient();
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
 
